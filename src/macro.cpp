0635311ed83b1a9753ad8be6e7f406388d5d15fc
Vinicius <viniciusangelus30@gmail.com>
2025-05-02 17:06:59 -0300

    update macro1.5

--- src/macro.cpp
+++ src/macro.cpp
@@ -7,9 +7,21 @@
 #include <array>
 #include <vector>
 #include <random>
+#include <sstream>
 
 using namespace std;
 
+vector<string> split(const string& str, char delimiter){
+    vector<string> arraySplit;
+
+    stringstream ss(str);
+    string item;
+
+    while(getline(ss, item, delimiter)){
+        arraySplit.push_back(item);
+    }
+    return arraySplit;
+}
 
 
 void setclipboard(const string& texto){
@@ -51,13 +63,70 @@ void insertText(string text){
 
 }
 
+void executeMacro(vector<string> arrayMacros, int lengthArray){
+    
+
+    string comandoAcao;
+    
+    string text; 
+    int x;
+    int y;
+
+    string stringX;
+    string stringY;
+    bool loopMacro = true;
+    while(loopMacro){
+
+        for(int i = 0;i<lengthArray;i++){
+            random_device rd;
+            random_device rd2;
+            
+            mt19937 gen(rd());
+            mt19937 gen2(rd2());
+            
+            uniform_int_distribution<> dist(1000,2000);
+            uniform_int_distribution<> dist2(300,600);
+            
+            int randomDiferencaAcao = dist(gen);
+            int randomDiferencaClick = dist2(gen2);
+            
+            
+            vector<string> arraySplit = split(arrayMacros[i], ' ');
+            
+            if(arraySplit[0] == "ENTER"){//primeiro comando é um click
+                stringX = arraySplit[1];
+                stringY = arraySplit[3];
+                
+                x = stoi(stringX);
+                y = stoi(stringY);
+                
+                SetCursorPos(x,y);
+                
+                Sleep(randomDiferencaClick);
+                mouse_event(MOUSEEVENTF_LEFTDOWN, 0,0,0,0);//CLICAR
+                Sleep(randomDiferencaClick);
+                mouse_event(MOUSEEVENTF_LEFTUP,0,0,0,0);//SOLTAR O CLICK
+                
+            }
+            if(arraySplit[0] == "TAB"){
+                string text = arraySplit[2];
+                Sleep(randomDiferencaClick);
+                insertText(text);
+            }
+            Sleep(randomDiferencaAcao);
+        }
+    }
+}
+
+
+
 void CreatMacro(vector<string>& arrayMacro){
     system("cls");
     char key;
     POINT cursorPos;
     int x;
     int y;
-    cout << "\n criar um macro, todos os comandos realizados serão salvos no macro\n";
+    cout << "\ncriar um macro, todos os comandos realizados serão salvos no macro\n";
     cout << "coloque o cursor na posicao!\naperte 'ENTER' para adicionar as cordenadas do cursor ao macro \n'TAB' para apertar uma tecla\n'ESC' para sair e salvar as ações ao macro";
     bool loopCreat = true;
     while(loopCreat){
@@ -126,7 +195,7 @@ void macro(){
         if(choice == 1){
             CreatMacro(macros);
         }if(choice == 2){
-            //executar o macro!
+            executeMacro(macros, macros.size());
         }
         
     }
